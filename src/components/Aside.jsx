81db7c0267da2d73284209ce8d74ad9b4cc8d082
Pandozavr <43937835+Pandozavr@users.noreply.github.com>
2020-05-05 16:35:39 +0300

    добавил файлы самурая

---
+++ src/components/Aside.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Aside = () => {
+  return (
+    <aside>
+      <div>
+        <a>My profile</a>
+      </div>
+      <div>
+        <a>Messeges</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+    </aside>
+  )
+}
+
+export default Aside;
---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main>
+      <div>
+        <img src='https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg' />
+      </div>
+      <div>ava + desc</div>
+      <div>
+        posts
+      <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </main>
+  )
+}
+
+export default Main;
