4c3755d1c8a44887a43fe38a28381b7c6bc3ec0a
Pandozavr <43937835+Pandozavr@users.noreply.github.com>
2020-12-18 19:22:27 +0300

    Updates

---
+++ precache-manifest.92d7b3fc0520a85bc2c118f3356a9059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8caa0ddcf41bd3543b67215ff10a610",
+    "url": "/samurai/index.html"
+  },
+  {
+    "revision": "41511f5c3d5c82ed2897",
+    "url": "/samurai/static/css/main.2285a52e.chunk.css"
+  },
+  {
+    "revision": "f7140f3ae649d2e859f9",
+    "url": "/samurai/static/js/2.f67662fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samurai/static/js/2.f67662fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41511f5c3d5c82ed2897",
+    "url": "/samurai/static/js/main.eaad0bcd.chunk.js"
+  },
+  {
+    "revision": "0c4491468b5c665b0593",
+    "url": "/samurai/static/js/runtime-main.b2db4cba.js"
+  },
+  {
+    "revision": "5ef146cc7ea91c1f12136cca748b2d92",
+    "url": "/samurai/static/media/Infinity200px.5ef146cc.svg"
+  },
+  {
+    "revision": "44330dbd1b68607423fba43ee3aad470",
+    "url": "/samurai/static/media/noAvatar.44330dbd.png"
+  }
+]);
